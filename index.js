44a038d3e764d7eb2d3cff1a1deb4eda94a31766
Matthew Kay <mkay0961@gmail.com>
2019-10-22 16:03:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
-
+katzDeliLine.push(name)
 console.log(`Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`)
 return (`Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`)
   }

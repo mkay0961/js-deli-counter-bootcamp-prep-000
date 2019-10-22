5edd8e76374e256fbb86255f7d0bc7f6c373185a
Matthew Kay <mkay0961@gmail.com>
2019-10-22 16:10:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ console.log(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
   }
 
-  function nowServing(katzDeli) {
+  function nowServing(katzDeliLine) {
+
+if (katzDeliLine.length === 0) {
+  return ("There is nobody waiting to be served!")
+  
+} else { for (var i = 0; i < katzDeliLine.length+ i++) {
+
+console.log(`Currently Serving ${katzDeliLine[i]}`)
+  return(`Currently Serving ${katzDeliLine[i]}`)
+}
+}
 
 
   }

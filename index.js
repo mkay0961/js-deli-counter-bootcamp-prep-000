9ea86ae2d53823dc7c16f5c71bc40b3aedfa571a
Matthew Kay <mkay0961@gmail.com>
2019-10-22 16:00:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
 
-return (`Welcome, ${name} You are number ${katzDeliLine.length} in line.`)
+return (`Welcome, ${name} You are number ${katzDeliLine.length + 1} in line.`)
   }
 
   function nowServing(katzDeli) {

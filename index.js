f15af1cf370d571eae41500c90462c8e9f5873af
Matthew Kay <mkay0961@gmail.com>
2019-10-22 15:59:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,30 +2,14 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
 
-katzDeliLine.push(`${name}`)
-return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+return (`Welcome, ${name} You are number ${katzDeliLine} in line.`)
   }
 
   function nowServing(katzDeli) {
-    if (katzDeli.length > 0) {
-      var nowServing = katzDeli[0];
-      katzDeli.shift();
-      return `Currently serving ${nowServing}.`;
-    } else {
-      return `There is nobody waiting to be served!`;
-    }
+
+
   }
 
   function currentLine(katzDeliLine){
-var currently = []
-    if (katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  } else {
 
-     for (var i = 0;i <katzDeliLine.length;i++ ) {
-    currently.push(` ${i+1}. ${katzDeliLine[i]}`)
-  }
-  console.log(currently)
-  return ("The line is currently:" + currently)
-  }
   }

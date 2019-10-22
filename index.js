21ffbb42e10b6222410ea62b613c5b4fa2ee0cea
Matthew Kay <mkay0961@gmail.com>
2019-10-22 19:18:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var array = []
     if (katzDeliLine.length === 0) {
       return ("The line is currently empty.")
     } else { for (var i = 0; i < katzDeliLine.length; i++)
-        array.push(`${i+1}. ${katzDeliLine[i]} `)
+        array.push(` ${i+1}. ${katzDeliLine[i]}`)
 
         return (`The line is currently: ${array}`)
 
